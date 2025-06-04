8dea1071e7f8d60face0d4d290016ac86d5fdb5c
Nemanja <nemanja.nikolic98@hotmail.com>
2025-06-04 20:14:39 +0200

    init

---
+++ influencer-sponsorship/src/main/java/cpm/influencer_sponsorship/influencer_sponsorship/InfluencerSponsorshipApplication.java
@@ -0,0 +1,13 @@
+package cpm.influencer_sponsorship.influencer_sponsorship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfluencerSponsorshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfluencerSponsorshipApplication.class, args);
+	}
+
+}
---
+++ influencer-sponsorship/src/test/java/cpm/influencer_sponsorship/influencer_sponsorship/InfluencerSponsorshipApplicationTests.java
@@ -0,0 +1,13 @@
+package cpm.influencer_sponsorship.influencer_sponsorship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfluencerSponsorshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
